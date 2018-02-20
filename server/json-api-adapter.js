3ebfbf1053b85bd5a342aa072af006024d80d03d
Jake Dluhy <dluhy.jake@gmail.com>
2018-02-19 18:37:06 -0800

    Fix json server for json api

--- server/json-api-adapter.js
+++ server/json-api-adapter.js
@@ -1,18 +1,28 @@
 const _ = require('lodash');
 
-const serializeObject = (obj) => ({
-  data: {
-    id:         obj.id,
-    type:       obj.objectType,
-    attributes: _.mapKeys(_.omit(obj, ['id', 'objectType']), (val, key) => _.kebabCase(key)),
-  },
+const getDataObject = (obj, type) => ({
+  type,
+  id:         obj.id,
+  attributes: _.mapKeys(_.omit(obj, ['id', 'objectType']), (val, key) => _.kebabCase(key)),
 });
+
+const getDataType = (url) => {
+  const noQuery = url.split('?')[0];
+
+  return _.last(
+    noQuery.split('/').filter((part) => !_.isNumber(part))
+  );
+}
 
 exports.serializeJsonapi = (req, res) => {
   const { data } = res.locals;
 
+  const type = getDataType(req.originalUrl);
+
   return res.json(
-    _.isArray(data) ? data.map(serializeObject) : serializeObject(data),
+    _.isArray(data) ?
+      { data: data.map((obj) => getDataObject(obj, type)) } :
+      { data: getDataObject(data, type) },
   );
 };
 
